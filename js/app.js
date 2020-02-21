f5a115bef2ddb4fec45c20ac7a8f19cf507caea4
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-21 14:01:52 +0000

    MVP completed

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newPlayerform = document.querySelector('#new-player-form')
+  newPlayerform.addEventListener('submit', addNewPlayerFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', deleteAllClick);
+})
+
+const addNewPlayerFormSubmit = function (event) {
+  event.preventDefault();
+
+  const playerListItem = createPlayer(event.target);
+  const playerList= document.querySelector('#players-list');
+  playerList.appendChild(playerListItem);
+
+  event.target.reset();
+}
+
+const createPlayer = function (form) {
+  const playerListItem = document.createElement('li');
+  playerListItem.classList.add('players-list-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  playerListItem.appendChild(name);
+
+  const sport = document.createElement('h3');
+  sport.textContent = form.sport.value;
+  playerListItem.appendChild(sport);
+
+  const continent = document.createElement('p');
+  continent.textContent = form.continent.value;
+  playerListItem.appendChild(continent);
+
+  return playerListItem;
+}
+
+const deleteAllButton = function (event) {
+  const playerList = document.querySelector('#players-list');
+  playerList.innerHTML = '';
+}
